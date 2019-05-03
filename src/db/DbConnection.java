7e9c21590f2cbfe097a6f920b2a471d8e0292a85
HuynhTuyetNhi <16110409@student.hcmute.edu.vn>
2019-05-03 20:20:10 +0700

    project

---
+++ src/db/DbConnection.java
@@ -0,0 +1,40 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+	static final String url = "jdbc:mysql://localhost:3306/mobile_store" + "?useUnicode=yes&characterEncoding=UTF-8";
+	static final String user = "root";
+	static final String password = "123456";
+
+	static DbConnection instance;
+
+	public static DbConnection getInstance() {
+		if (instance == null) {
+			instance = new DbConnection();
+		}
+		return instance;
+	}
+
+	private DbConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnection() {
+		Connection con = null;
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+}
